765b949c83a88fac6abf39837e43a3c7383bc226
Daniel E <daniel.e@cetastech.com>
2024-01-08 20:52:30 +0530

    browser build

--- main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-WYUXSHN4.mjs";import"./chunk-66S2TOVT.mjs";import"./chunk-4E6LMZRB.mjs";import"./chunk-6R33QTRJ.mjs";import"./chunk-MMEFXSLS.mjs";import"./chunk-BGVYGYKH.mjs";import"./chunk-JP7IJGBJ.mjs";import"./chunk-V7GNJKJX.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
